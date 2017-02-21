b796aa4b829bcf0e526d2601395409590698b9f7
kevin <kevinxpt27@gmail.com>
2017-02-21 11:15:52 -0500

    cambio de iconos y angular

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('view engine', 'jade');
 
 var appClientFiles = [
 'app_client/app.js',
-'app_client/home/home.controller.js',
+
 'app_client/about/about.controller.js',
 'app_client/auth/login/login.controller.js',
 'app_client/auth/register/register.controller.js',
